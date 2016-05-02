6a676b334067b93193b83ca2467239ae94cc2821
Dhaval <dhaval.vyomwork@gmail.com>
2016-05-02 17:45:54 +0530

    ACContollerCmmted

--- WebApplication1/Controllers/AccountController.cs
+++ WebApplication1/Controllers/AccountController.cs
@@ -58,7 +58,7 @@ namespace WebApplication1.Controllers
         public ActionResult Login(string returnUrl)
         {
             ViewBag.ReturnUrl = returnUrl;
-            return View();
+            return View(returnUrl);
         }
 
         //
